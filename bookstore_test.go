73920f1d9e73c82f9de08bfc1b674292045b5415
Sami <hellosami@protonmail.com>
2024-06-17 10:31:53 +0200

    Add test for function addBook()

---
+++ bookstore_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestAddBook(t *testing.T) {
+	var store bookStore
+	expected := book{
+		author: "Marshall B. Rosenberg",
+		title:  "nonviolent communication",
+		score:  7,
+	}
+	numberOfBooks := 1
+
+	store.addBook(expected)
+	actual := store.books[0]
+
+	if len(store.books) != numberOfBooks {
+		t.Errorf("Expected %d book. Actual: %d", numberOfBooks, len(store.books))
+	}
+	if store.books[0] != expected {
+		t.Errorf("Actual book does not match expected book. Expected: %+v, Actual: %+v", expected, actual)
+	}
+}
